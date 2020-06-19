2f49993b6e604985f438a78a47109418a64bf9ea
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2020-06-19 15:46:06 -0500

    add examples/namespaces/lazy
    Signed-off-by: Gregg Reynolds <601396+mobileink@users.noreply.github.com>

---
+++ examples/namespaces/lazy/alpha/driver.ml
@@ -0,0 +1,20 @@
+
+Alpha.Beta1.Gamma.Hello.msg() ;;
+
+open Alpha ;;
+Beta1.Gamma.Hello.msg()
+
+open Alpha.Beta1 ;;
+Gamma.Hello.msg()
+
+open Alpha.Beta1.Gamma.Hello ;;
+msg() ;;
+
+Alpha.Gamma.Howdy.msg() ;;
+
+open Alpha.Gamma ;;
+Howdy.msg()
+
+open Alpha.Gamma.Howdy ;;
+msg()
+
